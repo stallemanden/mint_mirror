461668844ce36ef06499293f0a1efbd82421c9d3
Torben Stallknecht <torben@hamdenkloge.dk>
2023-03-13 18:58:47 +0100

    Changes to Schedule for sync as well as an updated Readme.

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-echo $SYNC_MIRROR
-echo $SYNC_PACKAGES
-
 #Cear all Crontabs
 (crontab -r)
 
+#Enable Cronjob for sync of packages
 if [ "$SYNC_PACKAGES" = "true" ]; then
     echo "Setting up cronjob for sync of Mint Linux Packages..."
     (crontab -l ; echo "05 0,12 * * * /mint/files/sync_packages.sh") | crontab -
 fi
 
+#Enable Cronjob for sync of ISOs
 if [ "$SYNC_MIRROR" = "true" ]; then
     echo "Setting up cronjob for sync of Mint Linux ISO..."
-    (crontab -l ; echo "05 0 * * * /mint/files/sync_mirror.sh") | crontab -
+    (crontab -l ; echo "00 01 * * * /mint/files/sync_mirror.sh") | crontab -
 fi
